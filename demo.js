844bdb6e3462d184afadfebebe70e3ad73e22268
JoseAmieiro <joseperezamieiro@gmail.com>
2023-03-06 13:40:52 +0100

    ejercicio2 completo

---
+++ demo.js
@@ -0,0 +1,44 @@
+const product = { count: 1, price: 12.55, type: "ropa"};
+
+
+function getTotal (product) {
+    let precioSinIva; 
+    if(product.count < 0) {
+        precioSinIva = 0;
+    } else {
+        precioSinIva = product.count * product.price;
+    }
+    return precioSinIva;
+}
+
+function getVat (product) { 
+    let iva;
+    switch (product.type) {
+    case "alimentacion":
+        iva = 0.10;        
+        break;
+     case "libro":
+        iva = 0.04;        
+         break;
+     default: iva = 0.21;
+    }
+    return iva;
+}
+
+function getTotalVat(product) {
+console.log(getVat(product))
+    return product.count > 0 ? product.count * getVat(product) : 0;
+  
+}
+
+function printProductPrice(product) {
+    const subtotal = getTotal(product);
+    const vat = getTotalVat(product);
+    const total = subtotal + vat;
+  
+    console.log("Subtotal:", subtotal + "€");
+    console.log("IVA:", vat + "€");
+    console.log("Total:", total + "€");
+}
+
+printProductPrice(product);
